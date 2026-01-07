b18ec98c70c989dd494096b6fb7b3839e2a5a021
Gustavo Amarante <gusamarante@gmail.com>
2026-01-07 20:42:35 -0300

    Progress

--- examples/ff_5f.py
+++ examples/ff_5f.py
@@ -1,43 +1,14 @@
-from empfin import ff25p, ff5f, TimeseriesReg, CrossSectionReg
+from empfin import ff25p, ff5f, CrossSectionReg
 
-# TODO make a chart for the lambdas, similar to the alphas
 
 ports = ff25p()
 facts, rf = ff5f()
 
 # ===========================================
-# ===== Single Factor Model with TS Reg =====
-# ===========================================
-ts_reg_mkt = TimeseriesReg(
-    assets=ports,
-    factors=facts["Mkt-RF"],
-)
-
-# --- GRS test ---
-print(ts_reg_mkt.grs_test())
-ts_reg_mkt.plot_alpha_pred(title="Timeseries Regression - Single Factor Model")
-
-
-# ======================================
-# ===== 5 Factor Model with TS Reg =====
-# ======================================
-ts_reg_5f = TimeseriesReg(
-    assets=ports,
-    factors=facts,
-)
-
-# --- GRS test ---
-print(ts_reg_5f.grs_test())
-ts_reg_5f.plot_alpha_pred(title="Timeseries Regression - 5 Factor Model")
-
-
-# ===========================================
 # ===== Single Factor Model with CS Reg =====
 # ===========================================
-# cs_reg_mkt_noconst = CrossSectionReg(
-#     assets=ports,
-#     factors=facts,
-#     cs_const=False,
-# )
-
-# TODO Compare all the lambdas
+cs_reg_mkt_noconst = CrossSectionReg(
+    assets=ports,
+    factors=facts,
+    cs_const=False,
+)
