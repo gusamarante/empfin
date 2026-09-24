62ceab3b22a0acbb05d94718d69aa511fa52fe98
Gustavo Amarante <gusamarante@gmail.com>
2026-09-24 18:12:42 -0300

    adding TODOS

--- empfin/prinport.py
+++ empfin/prinport.py
@@ -2,7 +2,9 @@ import numpy as np
 import pandas as pd
 from numpy.linalg import eigh, svd
 
+# TODO replicate the charts of the paper
 # TODO add weight scaling by portfolio target volatility
+# TODO add regularization
 
 class PrincipalPortfolios:
     """
